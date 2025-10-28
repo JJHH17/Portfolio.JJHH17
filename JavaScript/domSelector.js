652b19d7364c0fd0cd8a6614d4395d2cf5cc7729
James Hatfield <james.hatfield17@icloud.com>
2025-10-28 21:49:57 +0000

    Added further dom selectors

--- JavaScript/domSelector.js
+++ JavaScript/domSelector.js
@@ -2,3 +2,7 @@
 
 export let aboutMeBtn = document.querySelector("#AboutMeBtn");
 export let aboutMeDiv = document.querySelector("#aboutMeDiv");
+export let projectsBtn = document.querySelector("#ProjectsBtn");
+export let projectsDiv = document.querySelector("#projectsDiv");
+export let contactBtn = document.querySelector("#ContactBtn");
+export let contactDiv = document.querySelector("#contactDiv");
