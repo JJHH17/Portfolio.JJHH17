4ca1cfad606aa044951466a54138e3446f598d29
James Hatfield <james.hatfield17@icloud.com>
2025-10-30 14:06:13 +0000

    Added functionality for projects button

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,15 @@
 // JavaScript for main page of Portoflio
 
-import { aboutMeBtn, aboutMeDiv, projectsDiv, contactDiv} from "./domSelector.js";
+import { aboutMeBtn, aboutMeDiv, projectsDiv, contactDiv, projectsBtn} from "./domSelector.js";
 
 aboutMeBtn.onclick = () => {
     aboutMeDiv.style.display = "block"
     projectsDiv.style.display = "none"
     contactDiv.style.display = "none"
 };
+
+projectsBtn.onclick = () => {
+    aboutMeDiv.style.display = "none"
+    projectsDiv.style.display = "block"
+    contactDiv.style.display = "none"
+}
